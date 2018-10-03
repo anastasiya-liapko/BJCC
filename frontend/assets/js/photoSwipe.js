9d62a928b188dc217d1cbe1fa48b930ff211bfd4
anastasiya-liapko <anastasiya.liapko@gmail.com>
2018-10-03 11:40:36 +0300

    Установила photoSwipe

--- frontend/assets/js/photoSwipe.js
+++ frontend/assets/js/photoSwipe.js
@@ -1,80 +1,131 @@
 'use strict';
 
-var pswpElement = document.querySelectorAll('.pswp')[0];
+(function($) {
+    var $pswp = $('.pswp')[0];
+    var image = [];
 
-// build items array
-var items = [
-    {
-        src: 'img/rectangle-00@3x.png',
-        w: 940,
-        h: 278
-    },
-    {
-        src: 'img/rectangle-13@3x.png',
-        w: 1374,
-        h: 840
-    },
-    {
-        src: 'img/rectangle-14@3x.png',
-        w: 736,
-        h: 644
-    }
-];
+    $('.picture').each( function() {
+        var $pic     = $(this),
+            getItems = function() {
+            
+                var items = [];
+                $pic.find('a').each(function() {
+                  
+                    var $href   = $(this).attr('href'),
+                        $size   = $(this).data('size').split('x'),
+                        $width  = $size[0],
+                        $height = $size[1];
 
-// define options (if needed)
-var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0, // start at first slide
-};
+                    var item = {
+                        src : $href,
+                        w   : $width,
+                        h   : $height
+                    }
 
-if ( $(window).width() < 576 ) {
-  document.querySelector('.my-gallery').onclick = function(evt) {
-    evt.preventDefault();
-    var elem = evt.target;
-      if (elem.hasAttribute('src')) {
-      // Initializes and opens PhotoSwipe
-      var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-      gallery.init();
-    }
-  } 
-} 
-if ( $(window).width() > 576 ) {
-    $('.my-gallery').click( function(evt) { // лoвим клик пo ссылки с id="go"
-        event.preventDefault(); // выключaем стaндaртную рoль элементa
-        var elem = evt.target;
-        if (elem.hasAttribute('src')) {
-          if ( $(window).width() > 576 ) {
-              $('#myModal').modal('show')
-          }
-        }
-    })
-}
+                    items.push(item);
+                });
+                return items;
+            }
 
-$(window).resize(function() {
+        var items = getItems();
+        
+        $.each(items, function(index, value) {
+            image[index]     = new Image();
+            image[index].src = value['src'];
+        });
 
-    if ( $(window).width() < 576 ) {
-      document.querySelector('.my-gallery').onclick = function(evt) {
-      evt.preventDefault();
-        var elem = evt.target;
-        if (elem.hasAttribute('src')) {
-          if ( $(window).width() < 576 ) {
-            // Initializes and opens PhotoSwipe
-            var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-            gallery.init();
-          }
-        }
-      } 
+        $pic.on('click', 'figure', function(event) {
+            event.preventDefault();
+            
+            var $index = $(this).index();
+            var options = {
+                index: $index,
+                bgOpacity: 0.95,
+                showHideOpacity: true
+            }
 
-    } else {
-      $('.my-gallery').click( function(evt) { // лoвим клик пo ссылки с id="go"
-        event.preventDefault(); // выключaем стaндaртную рoль элементa
-        var elem = evt.target;
-        if (elem.hasAttribute('src')) {
-          if ( $(window).width() > 576 ) {
-              $('#myModal').modal('show')
-          } 
-        }
-      })
-    }
-});
+            var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+            lightBox.init();
+        });
+    });
+})(jQuery);
+
+
+// var pswpElement = document.querySelectorAll('.pswp')[0];
+
+// // build items array
+// var items = [
+//     {
+//         src: 'img/rectangle-00@3x.png',
+//         w: 940,
+//         h: 278
+//     },
+//     {
+//         src: 'img/rectangle-13@3x.png',
+//         w: 1374,
+//         h: 840
+//     },
+//     {
+//         src: 'img/rectangle-14@3x.png',
+//         w: 736,
+//         h: 644
+//     }
+// ];
+
+// // define options (if needed)
+// var options = {
+//     // optionName: 'option value'
+//     // for example:
+//     index: 0, // start at first slide
+// };
+
+// if ( $(window).width() < 576 ) {
+//   document.querySelector('.my-gallery').onclick = function(evt) {
+//     evt.preventDefault();
+//     var elem = evt.target;
+//       if (elem.hasAttribute('src')) {
+//       // Initializes and opens PhotoSwipe
+//       var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+//       gallery.init();
+//     }
+//   } 
+// } 
+// if ( $(window).width() > 576 ) {
+//     $('.my-gallery').click( function(evt) { // лoвим клик пo ссылки с id="go"
+//         event.preventDefault(); // выключaем стaндaртную рoль элементa
+//         var elem = evt.target;
+//         if (elem.hasAttribute('src')) {
+//           if ( $(window).width() > 576 ) {
+//               $('#myModal').modal('show')
+//           }
+//         }
+//     })
+// }
+
+// $(window).resize(function() {
+
+//     if ( $(window).width() < 576 ) {
+//       document.querySelector('.my-gallery').onclick = function(evt) {
+//       evt.preventDefault();
+//         var elem = evt.target;
+//         if (elem.hasAttribute('src')) {
+//           if ( $(window).width() < 576 ) {
+//             // Initializes and opens PhotoSwipe
+//             var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+//             gallery.init();
+//           }
+//         }
+//       } 
+
+//     } else {
+//       $('.my-gallery').click( function(evt) { // лoвим клик пo ссылки с id="go"
+//         event.preventDefault(); // выключaем стaндaртную рoль элементa
+//         var elem = evt.target;
+//         if (elem.hasAttribute('src')) {
+//           if ( $(window).width() > 576 ) {
+//               $('#myModal').modal('show')
+//           } 
+//         }
+//       })
+//     }
+// });
